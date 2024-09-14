4d1965bf43743f8b2972b912a6411dcdbe410da3
nnimpit <natthaphob@gmail.com>
2024-09-14 21:29:19 +0700

    add simple subscriber.py

--- src/smartbot_py_examples/setup.py
+++ src/smartbot_py_examples/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simple_publisher = smartbot_py_examples.simple_publisher:main'
+            'simple_publisher = smartbot_py_examples.simple_publisher:main',
+            'simple_subscriber = smartbot_py_examples.simple_subscriber:main'
         ],
     },
 )
